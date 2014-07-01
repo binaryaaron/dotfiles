45c0aa5472678e47e44489ec3fa93df62978d9bd
Aaron Gonzales <xysmas@gmail.com>
2014-07-01 13:30:59 -0600

    some tweaks

--- initdotfiles.sh
+++ initdotfiles.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo initializing dotfiles 
 if [ ! -d "$HOME/dotfiles/" ]; then
-	cd ~/
+	cd $HOME
 	git clone https://github.com/xysmas/dotfiles.git
-	echo i'm in `pwd`
-	git clone https://github.com/xysmas/dotfiles.git
+fi
+
+if [ -d "$HOME/dotfiles" ]; then
 	ln -s ~/dotfiles/.gitconfig ~/.gitconfig 
 	ln -s ~/dotfiles/.bashrc ~/.bashrc 
 	ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases 
