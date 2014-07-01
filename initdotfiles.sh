61d5ec5e2ba69a4fa608b8ae8de56d96775c4f07
Aaron Gonzales <xysmas@gmail.com>
2014-07-01 12:20:36 -0600

    changed init script

--- initdotfiles.sh
+++ initdotfiles.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd ~/dotfiles
-
-git clone https://github.com/xysmas/dotfiles.git
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig 
-ln -s ~/dotfiles/.bashrc ~/.bashrc 
-ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases 
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile 
-
-
+echo initializing dotfiles 
+if [ ! -d "~/dotfiles/" ]; then
+	cd ~/
+	git clone https://github.com/xysmas/dotfiles.git
+	echo i'm in `pwd`
+	git clone https://github.com/xysmas/dotfiles.git
+	ln -s ~/dotfiles/.gitconfig ~/.gitconfig 
+	ln -s ~/dotfiles/.bashrc ~/.bashrc 
+	ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases 
+	ln -s ~/dotfiles/.bash_profile ~/.bash_profile 
+fi
