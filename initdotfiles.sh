80981c69e1fb11414040faa673cf0e61179e6c49
Aaron Gonzales <xysmas@gmail.com>
2014-08-04 22:41:53 -0600

    added symlink for gitignore

--- initdotfiles.sh
+++ initdotfiles.sh
@@ -8,6 +8,7 @@ fi
 if [ -d "$HOME/dotfiles" ]; then
 	echo 'making symlinks'
 	ln -s ~/dotfiles/.gitconfig ~/.gitconfig 
+	ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global 
 	ln -s ~/dotfiles/.bashrc ~/.bashrc 
 	ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases 
 	ln -s ~/dotfiles/.bash_profile ~/.bash_profile 
