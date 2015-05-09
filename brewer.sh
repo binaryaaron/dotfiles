ce5493798bc05b59ae0137e7b0d8fe99bccce1ca
Aaron Gonzales <xysmas@gmail.com>
2015-05-08 19:44:09 -0600

    added mac osx brewer stuff

---
+++ brewer.sh
@@ -0,0 +1,59 @@
+brew tap caskroom/cask
+brew install brew-cask
+brew cask install dropbox
+brew cask install vlc
+brew install git
+git --version
+which git
+brew install python3
+pip install --upgrade setuptools
+pip3 install --upgrade setuptools
+pip3 install --upgrade pip
+pip install virtualenv
+pip3 install virtualenv
+brew tap samueljohn/python
+brew tap homebrew/science
+brew install gfortran
+brew install gcc
+# brew install gcc
+# brew tap samueljohn/python
+# brew tap homebrew/science
+brew update 
+pip3 install nose
+pip3 install pyparsing
+pip3 install python-dateutil
+pip3 install pep8
+pip3 install numpy
+pip3 install scipy
+pip3 install matplotlib
+
+pip3 install zeromq # Necessary for pyzmq
+pip3 install pyqt # Necessary for the qtconsole
+pip3 install pyzmq
+pip3 isntall pygments
+pip3 install jinja2
+pip3 install tornado
+pip3 install ipython3
+pip3 install jsonschema
+pip3 install ipython3-notebook
+pip3 install pandas
+pip3 install gensim
+
+pip3 install symPy
+pip3 install statsmodels
+pip3 install shogun
+pip3 install pyBrain
+pip3 install pyLearn2
+pip3 install pyMC
+pip3 install bokeh
+pip3 install d3py
+pip3 install ggplot
+pip3 install matplotlib
+pip3 install plotly
+pip3 install prettyplotlib
+pip3 install seaborn
+pip3 install csvkit
+pip3 install tables
+pip3 install sqlite3
+pip3 install theano
+pip3 install keras
