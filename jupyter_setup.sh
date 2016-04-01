f9341a1c8ca7f14001365257737fa23fc81c410c
Aaron Gonzales <xysmas@gmail.com>
2016-04-01 00:15:53 -0600

    added config for jupyter vim bindings

---
+++ jupyter_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# installs vim extensions for jupyter; requires jupyter
+if [ -d $(jupyter --data-dir) ]; then
+    mkdir -p $(jupyter --data-dir)/nbextensions
+    cd $(jupyter --data-dir)/nbextensions
+    git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+    jupyter nbextension enable vim_binding/vim_binding
+fi
