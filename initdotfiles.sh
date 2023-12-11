69dbed73d16cd47b4487866141c1c467b9bf1d40
Aaron Gonzales <aaron@aarongonzales.net>
2023-12-11 10:10:28 -0700

    Update initdotfiles.sh

--- initdotfiles.sh
+++ initdotfiles.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+
+mkdir -p "$HOME/dotfiles"
+cd "$HOME/dotfiles"
+bash install_packages.sh
+
 if [ -d "$HOME/dotfiles" ]; then
 	echo 'making symlinks'
+	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	gh repo clone romkatv/powerlevel10k $ZSH_CUSTOM/themes/powerlevel10k
+
 	ln -s ~/dotfiles/.gitconfig ~/.gitconfig 
 	ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global 
-	ln -s ~/dotfiles/.bashrc ~/.bashrc 
-	ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases 
-	ln -s ~/dotfiles/.bash_profile ~/.bash_profile 
-	ln -s ~/dotfiles/.dircolors ~/.dircolors
-	ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
+	ln -s ~/dotfiles/.zshenv ~/.zshenv
+ 	ln -s ~/dotfiles/.zshrc ~/.zshrc
+  	ln -s ~/dotfiles/.zshprofile ~/.zshprofile
+  
 fi
-	
+
+
