c4b482dd028369c658ff2249fe485a6280030ab9
Aaron Gonzales <aagonzales@nvidia.com>
2025-07-02 08:17:49 -0600

    adding bazel aliases

--- bazel_aliases.sh
+++ bazel_aliases.sh
@@ -1,4 +1,9 @@
+alias bq='bazel query '
 alias bq-ob='bazel query --output=build '
 alias bq-oj='bazel query --output=streamed_jsonproto '
+alias bq-op='bazel query --output=streamed_proto '
+alias bq-olk='bazel query --output=label_kind '
 alias bq-o='bazel query --output '
+alias bcq='bazel cquery '
+alias bcq-o='bazel cquery --output '
 
